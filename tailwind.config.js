9688226ac869a5bd71b6fd9f71057422f60af403
victormhp <victoralbertomhp@gmail.com>
2023-07-05 11:10:43 -0600

    Add custom box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/client/**/*.{js,jsx,ts,tsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'error-100': 'inset 0 0 0 1px #ef4444',
+        'error-150': 'inset 0 0 0 1.5px #ef4444',
+        'valid-100': 'inset 0 0 0 1px #a1a1aa',
+        'valid-150': 'inset 0 0 0 1.5px #a1a1aa',
+        input: 'inset 0 0 0 1px #3f3f46',
+      },
+    },
   },
   plugins: [],
 };
