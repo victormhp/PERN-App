3e86dd515e282762e4fb2e8e3fa29edd1e46a71f
victormhp <victoralbertomhp@gmail.com>
2023-07-05 11:15:42 -0600

    Add page not found

---
+++ src/client/routes/PageNotFound.tsx
@@ -0,0 +1,15 @@
+function PageNotFound() {
+  return (
+    <main className='grid min-h-screen place-content-center'>
+      <div className='max-w-md rounded-md border border-zinc-700 bg-zinc-800 p-8 shadow-md'>
+        <h1 className='mb-4 text-3xl font-semibold'>404 - Page Not Found</h1>
+        <p className='mb-4'>Oops! The page you are looking for does not exist.</p>
+        <a href='/' className='text-purple-400 hover:underline'>
+          Go back to homepage
+        </a>
+      </div>
+    </main>
+  );
+}
+
+export default PageNotFound;
