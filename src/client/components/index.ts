be54ff4d6d3d713e8c232de06e2bfba0b5ae4fd7
victormhp <victoralbertomhp@gmail.com>
2023-07-05 11:14:59 -0600

    Export icons

---
+++ src/client/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Icons';
+export { default as Menu } from './Menu';
+export { default as Navigation } from './Navigation';
+export { default as Notes } from './Notes';
+export { default as Input } from './UI/Input';
+export { default as MenuButton } from './UI/MenuButton';
+export { default as NavButton } from './UI/NavButton';
