ed9dd7e6ec7bf6a6f9e02d9b86d8662b088b52ac
victormhp <victoralbertomhp@gmail.com>
2023-07-05 11:18:51 -0600

    Export routes

---
+++ src/client/routes/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthLayout } from './AuthLayout';
+export { default as DashLayout } from './DashLayout';
+export { default as Login } from './Login';
+export { default as PageNotFound } from './PageNotFound';
+export { default as Register } from './Register';
