5bb679bf9f8f8ccebc9e3904e6ae63438d41cd0f
victormhp <victoralbertomhp@gmail.com>
2023-07-05 11:14:52 -0600

    Add router

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,40 @@
-function App(): JSX.Element {
-  return <h1 className='bg-red-300 text-3xl font-bold underline text-red-600'>Simple React Typescript Tailwind Sample</h1>;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { PageNotFound, DashLayout, Login, Register, AuthLayout } from './routes';
+import { Notes } from './components';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        Component: Login,
+      },
+      {
+        path: '/register',
+        Component: Register,
+      },
+    ],
+  },
+  {
+    path: '/dash',
+    Component: DashLayout,
+    children: [
+      {
+        path: '/dash',
+        Component: Notes,
+      },
+    ],
+  },
+  {
+    path: '*',
+    Component: PageNotFound,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
