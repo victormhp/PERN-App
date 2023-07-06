f223d415f06dd1f456338b3f78212d3476aa1554
victormhp <victoralbertomhp@gmail.com>
2023-07-06 00:13:57 -0600

    Update Link

--- src/client/routes/PageNotFound.tsx
+++ src/client/routes/PageNotFound.tsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function PageNotFound() {
   return (
     <main className='grid min-h-screen place-content-center'>
       <div className='max-w-md rounded-md border border-zinc-700 bg-zinc-800 p-8 shadow-md'>
         <h1 className='mb-4 text-3xl font-semibold'>404 - Page Not Found</h1>
         <p className='mb-4'>Oops! The page you are looking for does not exist.</p>
-        <a href='/' className='text-purple-400 hover:underline'>
+        <Link to='/' className='text-purple-400 hover:underline'>
           Go back to homepage
-        </a>
+        </Link>
       </div>
     </main>
   );
