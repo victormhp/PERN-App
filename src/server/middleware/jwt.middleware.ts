1696c2623674b82321efc23b3e91a0f98b4a6cde
victormhp <victoralbertomhp@gmail.com>
2023-07-06 00:14:59 -0600

    Change jwt middleware name

---
+++ src/server/middleware/jwt.middleware.ts
@@ -0,0 +1,25 @@
+import { type Response, type NextFunction, type Request } from 'express';
+import { type Secret, verify } from 'jsonwebtoken';
+import { type PayloadToken } from '../types/custom';
+import 'dotenv/config';
+
+function verifyJWT(req: Request, res: Response, next: NextFunction): void {
+  const authHeader = req.headers.authorization;
+  const validAuthHeader = authHeader?.startsWith('Bearer ');
+
+  if (typeof validAuthHeader === 'boolean' && !validAuthHeader) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
+  }
+
+  const secret: Secret = process.env.ACCESS_TOKEN_SECRET ?? '';
+  const token: string = authHeader?.split(' ')[1] ?? '';
+
+  verify(token, secret, (err, decoded) => {
+    if (err != null) return res.status(403).json({ message: 'Forbidden' });
+    req.user = (decoded as PayloadToken).username;
+    next();
+  });
+}
+
+export default verifyJWT;
