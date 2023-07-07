afdda82bc0a3b49fefaecc87ffbee2f87195fa0f
victormhp <victoralbertomhp@gmail.com>
2023-07-06 22:46:17 -0600

    Update exports

--- src/client/routes/index.ts
+++ src/client/routes/index.ts
@@ -1,5 +1,7 @@
 export { default as AuthLayout } from './AuthLayout';
 export { default as DashLayout } from './DashLayout';
+export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as PageNotFound } from './PageNotFound';
+export { default as ProtectedRoutes } from './ProtectedRoutes';
 export { default as Register } from './Register';
