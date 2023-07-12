50801204854c58ce357c4cc7659d5ef5599afdbf
victormhp <victoralbertomhp@gmail.com>
2023-07-12 00:24:05 -0600

    Update frontend models

---
+++ src/client/models/auth.models.ts
@@ -0,0 +1,10 @@
+export interface RegisterCredentials {
+  email: string;
+  password: string;
+  username: string;
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
---
+++ src/client/models/note.models.ts
@@ -0,0 +1,10 @@
+export interface Note {
+  userId: number;
+  title: string;
+  description: string;
+}
+
+export interface NewNote {
+  title: string;
+  description: string;
+}
