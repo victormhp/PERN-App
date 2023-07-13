227bf17f6d2089394ae31e9e822257ff968837a6
victormhp <victoralbertomhp@gmail.com>
2023-07-13 16:59:25 -0600

    Update icon exports

--- src/client/components/Icons/index.ts
+++ src/client/components/Icons/index.ts
@@ -4,6 +4,7 @@ export { default as GridViewIcon } from './GridViewIcon';
 export { default as HidePasswordIcon } from './HidePasswordIcon';
 export { default as InvalidIcon } from './InvalidIcon';
 export { default as LightModeIcon } from './LightModeIcon';
+export { default as ListViewIcon } from './ListViewIcon';
 export { default as MenuIcon } from './MenuIcon';
 export { default as NotesIcon } from './NotesIcon';
 export { default as ShowPasswordIcon } from './ShowPasswordIcon';
