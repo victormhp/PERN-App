6fdc1f3417834a068f9fefa669340bfedd657790
victormhp <victoralbertomhp@gmail.com>
2023-07-13 16:58:32 -0600

    Update exports

--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -3,6 +3,8 @@ export { default as Menu } from './Menu';
 export { default as NavLoggedIn } from './NavLoggedIn';
 export { default as NavLoggedOut } from './NavLoggedOut';
 export { default as Notes } from './Notes';
+export { default as NotesForm } from './NotesForm';
+export { default as NotesList } from './NotesList';
 export { default as Input } from './UI/Input';
 export { default as MenuButton } from './UI/MenuButton';
 export { default as NavButton } from './UI/NavButton';
--- src/client/routes/index.ts
+++ src/client/routes/index.ts
@@ -3,5 +3,6 @@ export { default as DashLayout } from './DashLayout';
 export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as PageNotFound } from './PageNotFound';
+export { default as PersistLogin } from './PersistLogin';
 export { default as ProtectedRoutes } from './ProtectedRoutes';
 export { default as Register } from './Register';
