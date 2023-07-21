c68da09e4deef7618396016dfc30db70fb639507
victormhp <victoralbertomhp@gmail.com>
2023-07-20 22:48:43 -0600

    Fix nav link

--- src/client/components/NavAuth.tsx
+++ src/client/components/NavAuth.tsx
@@ -4,7 +4,7 @@ function NavAuth() {
   return (
     <nav className='flex items-center justify-between px-4'>
       <div className='flex items-center gap-x-2  sm:gap-x-4'>
-        <NavLink to='#' className='inline-flex items-center'>
+        <NavLink to='/' className='inline-flex items-center'>
           <img src='/logo.png' alt='Logo' className='h-[54px] w-[54px] p-2' />
           <h1 className='hidden text-2xl font-semibold tracking-wide text-purple-500 sm:block'>
             PERN <span className='font-medium text-primary'>Notes</span>
